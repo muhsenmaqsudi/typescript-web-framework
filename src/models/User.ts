838672103ce26f72cbd3ab320c76d08f714bd7c2
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2019-12-12 20:24:16 +0330

    Sync class, a more complicated extraction

--- src/models/User.ts
+++ src/models/User.ts
@@ -19,21 +19,4 @@ export class User {
   set(update: UserProps): void {
     Object.assign(this.data, update);
   }
-
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get("id")}`)
-      .then((res: AxiosResponse): void => {
-        this.set(res.data);
-      });
-  }
-
-  save(): void {
-    const id = this.get("id");
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      axios.post("http://localhost:3000/users", this.data);
-    }
-  }
 }

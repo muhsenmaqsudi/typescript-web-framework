8547f95aab84c485fd02ac63f8dea4643a86046e
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2019-12-13 15:40:38 +0330

    final user refactor

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 1, name: "newer name", age: 1 });
+const user = User.buildUser({ id: 1 });
 
 user.on("save", () => {
   console.log(user);
 });
 
-user.save();
+user.fetch();
